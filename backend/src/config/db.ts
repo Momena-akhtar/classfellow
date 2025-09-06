62dd0ac9584101fdf296374929b5c46f16769670
Momena <momena.akhtar19@gmail.com>
2025-09-06 22:28:38 +0500

    configure database

---
+++ backend/src/config/db.ts
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_DB_URI || "mongodb://localhost:27017/classfellow");
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+  }
+};
